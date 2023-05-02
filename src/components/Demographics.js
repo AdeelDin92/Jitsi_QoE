79a0393c9c5d98c7269284daa3d41b51c5ceaf7c
AdeelDin92 <adeeldin@outlook.de>
2023-05-02 12:42:35 +0200

    Updated

--- src/components/Demographics.js
+++ src/components/Demographics.js
@@ -29,7 +29,7 @@ function Demographics() {
     e.preventDefault()
 
 
-    axios.post("http://localhost:4000/Demographics", Questions)
+    axios.post("https://jitsi-backend.onrender.com/", Questions)
       .then(function (response) {
         console.log(response.data)
         if(response.status = 200) {
