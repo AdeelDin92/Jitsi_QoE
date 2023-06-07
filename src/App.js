b2bca4e27b822e3b56c21a72a0885ddc3eafe4f9
AdeelDin92 <adeeldin@outlook.de>
2023-06-07 13:36:12 +0200

    Changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Routing from "./components/Routing"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { JitsiMeeting } from '@jitsi/react-sdk';
+
 
 
 function App() {
